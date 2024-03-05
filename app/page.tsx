3513692b7da566fc03184939158435db8599ec2b
Aidan Hendrickson <aidhendrickson@gmail.com>
2024-03-04 23:02:31 -0700

    Updates to various plants

--- app/page.tsx
+++ app/page.tsx
@@ -9,40 +9,42 @@ import PlantList from "components/PlantList/PlantList";
 import "./Calendar.scss";
 
 const Calendar = () => {
-  const [plants, setPlants] = useState<PlantData[]>([]);
-  const currentMonth = getCurrentMonth();
+    const [plants, setPlants] = useState<PlantData[]>([]);
+    const currentMonth = getCurrentMonth();
 
-  useEffect(() => {
-    const loadAndSetPlants = async () => {
-      const plantListResponse = await fetch("/assets/plantList.yml");
-      const plantListYAML = await plantListResponse.text();
-      const plantNames: string[] = YAML.load(plantListYAML);
+    useEffect(() => {
+        const loadAndSetPlants = async () => {
+            const plantListResponse = await fetch("/plantList.yml");
+            const plantListYAML = await plantListResponse.text();
+            const plantNames: string[] = YAML.load(plantListYAML);
 
-      const newPlants: PlantData[] = [];
-      for (const plantName of plantNames) {
-        const plantData: PlantData = await loadData(plantName);
-        if (
-          (plantData.start <= currentMonth && plantData.end >= currentMonth) ||
-          // account for winter plants with season including the new year
-          (plantData.end < plantData.start &&
-            (plantData.start <= currentMonth || plantData.end >= currentMonth))
-        ) {
-          plantData.dirName = plantName;
-          newPlants.push(plantData);
-        }
-      }
-      setPlants(newPlants);
-    };
+            const newPlants: PlantData[] = [];
+            for (const plantName of plantNames) {
+                const plantData: PlantData = await loadData(plantName);
+                if (
+                    (plantData.start <= currentMonth &&
+                        plantData.end >= currentMonth) ||
+                    // account for winter plants with season including the new year
+                    (plantData.end < plantData.start &&
+                        (plantData.start <= currentMonth ||
+                            plantData.end >= currentMonth))
+                ) {
+                    plantData.dirName = plantName;
+                    newPlants.push(plantData);
+                }
+            }
+            setPlants(newPlants);
+        };
 
-    loadAndSetPlants();
-  }, []);
+        loadAndSetPlants();
+    }, []);
 
-  return (
-    <div className="calendar">
-      <h2>What's in Season in {getMonthName(currentMonth)}?</h2>
-      <PlantList plants={plants} />
-    </div>
-  );
+    return (
+        <div className="calendar">
+            <h2>What's in Season in {getMonthName(currentMonth)}?</h2>
+            <PlantList plants={plants} />
+        </div>
+    );
 };
 
 export default Calendar;
