60ab32210c5a6ea0832be334712484723f3c0513
Aidan Hendrickson <aidhendrickson@gmail.com>
2024-03-04 23:03:27 -0700

    Adding opuntia, moving plantList

--- app/all-plants/page.tsx
+++ app/all-plants/page.tsx
@@ -8,35 +8,35 @@ import PlantGrid from "components/PlantGrid/PlantGrid";
 import "./FullPlantList.scss";
 
 const FullPlantList = () => {
-  const [plants, setPlants] = useState([]);
-  const currentMonth = getCurrentMonth();
+    const [plants, setPlants] = useState([]);
+    const currentMonth = getCurrentMonth();
 
-  useEffect(() => {
-    const loadAndSetPlants = async () => {
-      const plantListResponse = await fetch("/assets/plantList.yml");
-      const plantListYAML = await plantListResponse.text();
-      const plantNames = YAML.load(plantListYAML);
+    useEffect(() => {
+        const loadAndSetPlants = async () => {
+            const plantListResponse = await fetch("/plantList.yml");
+            const plantListYAML = await plantListResponse.text();
+            const plantNames = YAML.load(plantListYAML);
 
-      const newPlants = await Promise.all(
-        plantNames.map(async (plantName) => {
-          const plantData = await loadData(plantName);
-          plantData.dirName = plantName;
-          return plantData;
-        })
-      );
+            const newPlants = await Promise.all(
+                plantNames.map(async (plantName) => {
+                    const plantData = await loadData(plantName);
+                    plantData.dirName = plantName;
+                    return plantData;
+                })
+            );
 
-      setPlants(newPlants);
-    };
+            setPlants(newPlants);
+        };
 
-    loadAndSetPlants();
-  }, []);
+        loadAndSetPlants();
+    }, []);
 
-  return (
-    <div className="calendar">
-      <h2>All Plants:</h2>
-      <PlantGrid plants={plants} />
-    </div>
-  );
+    return (
+        <div className="calendar">
+            <h2>All Plants:</h2>
+            <PlantGrid plants={plants} />
+        </div>
+    );
 };
 
 export default FullPlantList;
